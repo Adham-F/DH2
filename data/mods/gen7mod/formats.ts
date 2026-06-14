141a88b9f241c770ad74d478ceabcdcf31ede622
Adham Fulton <fultonadham@gmail.com>
2026-06-14 13:01:43 -0400

    Son

--- data/mods/gen7mod/formats.ts
+++ data/mods/gen7mod/formats.ts
@@ -1,6 +1,6 @@
 export const Formats: import('../../../sim/dex-formats').FormatData[] = [
 	{
-		name: "[Gen 7] Gen 7 Mod",
+		name: "[Gen 7] Mod",
 		desc: `Custom Gen 7 format with additional Mega Evolutions and features.`,
 		mod: 'gen7mod',
 		ruleset: ['Standard', 'Mega Rayquaza Clause', 'Z-Move Clause'],
